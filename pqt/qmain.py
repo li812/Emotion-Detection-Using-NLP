0957fb5cdbbbe7e3df396e12020caeb56508d672
Ali Ahammad <68907011+li812@users.noreply.github.com>
2023-05-11 15:28:48 +0530

    Update qmain.py

--- pqt/qmain.py
+++ pqt/qmain.py
@@ -8,6 +8,7 @@ import sounddevice
 from scipy.io.wavfile import write
 import soundfile
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
+from PyQt5.QtGui import QFont
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
 import speech_recognition as sr
@@ -15,56 +16,58 @@ import speech_recognition as sr
 class Main(QWidget):
     def __init__(self):
         super().__init__()
+
         self.duration = QLineEdit(self)
         self.fpath = QLineEdit(self)
         self.lbl_text = QLabel(self)
-        self.lbl_text.setText("waiting")
+
+
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Admin Home")
-        self.setGeometry(100, 100, 1200, 700)
-        large_font = ('Verdana', 15)
+        self.setWindowTitle("Emotion-Detection-Using-NLP")
+        self.setGeometry(100, 100, 290, 525)
+        large_font = QFont('Verdana', 15)
 
-        lbl_2 = QLabel("Duration in Seconds", self)
-        lbl_2.move(10, 10)
+        lbl_2 = QLabel("Duration in Seconds   ", self)
+        lbl_2.move(20, 20)
         lbl_2.resize(250, 50)
-        self.duration.move(400, 20)
+
+        self.duration.move(150, 20)
         self.duration.resize(50, 50)
 
         sbmitbtn = QPushButton("RECORD VOICE", self)
-        sbmitbtn.move(200, 75)
+        sbmitbtn.move(20, 100)
         sbmitbtn.resize(250, 50)
         sbmitbtn.setFont(large_font)
         sbmitbtn.clicked.connect(self.recordvoice)
 
         lbl = QLabel("Select a Voice...", self)
-        lbl.move(200, 200)
+        lbl.move(20, 150)
         lbl.resize(250, 50)
-        self.lbl_text.move(900, 200)
-        self.lbl_text.resize(250, 50)
 
-        txt = QLineEdit(self)
-        txt.move(200, 300)
-        txt.resize(250, 50)
-        txt.setText("")
-        self.fpath.move(200, 300)
+       
+
+    
+        self.fpath.move(20, 200)
         self.fpath.resize(250, 50)
 
+
+
         browse = QPushButton("Browse", self)
-        browse.move(700, 300)
+        browse.move(20, 250)
         browse.resize(250, 50)
         browse.setFont(large_font)
         browse.clicked.connect(self.browsefunc)
 
         emotion = QPushButton("VIEW EMOTION", self)
-        emotion.move(200, 400)
+        emotion.move(20, 350)
         emotion.resize(250, 50)
         emotion.setFont(large_font)
         emotion.clicked.connect(self.viewemotion)
 
         ext = QPushButton("Exit", self)
-        ext.move(200, 550)
+        ext.move(20, 450)
         ext.resize(250, 50)
         ext.setFont(large_font)
         ext.clicked.connect(self.close)
